7613bd40a11bc0ccfa3cbb3b05c0dcc8d29ee00c
Mohamad Supangat <hantamkoding@gmail.com>
2022-04-25 19:20:24 +0700

    testing penggunaan rest dengan socket io dan ternyata work

---
+++ index.js
@@ -0,0 +1,29 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.get("/", function (req, res) {
+  res.sendfile("index.html");
+});
+
+//Whenever someone connects this gets executed
+io.on("connection", function (socket) {
+  console.log("A user connected");
+
+  //Whenever someone disconnects this piece of code executed
+  io.emit("new_message", {
+    testing: "iya hanya testing",
+  });
+  socket.on("disconnect", function () {
+    console.log("A user disconnected");
+  });
+});
+
+app.route("/test").get(function (req, res) {
+  io.emit("new_message", "world");
+  res.end();
+});
+
+http.listen(3000, function () {
+  console.log("listening on *:3000");
+});
